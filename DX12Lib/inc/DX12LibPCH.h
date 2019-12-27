ecda64469ecba453daf8062ef37470459bf77742
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2019-12-27 13:35:06 +0100

    Adding D3DX12AffinityLayer library.

--- DX12Lib/inc/DX12LibPCH.h
+++ DX12Lib/inc/DX12LibPCH.h
@@ -48,8 +48,8 @@
 using namespace Microsoft::WRL;
 
 // DirectX 12 specific headers.
-#include "d3dx12.h"
 #include <dxgi1_6.h>
+#include <d3dx12affinity_d3dx12.h>
 #include <d3dcompiler.h>
 #include <DirectXMath.h>
 #include <DirectXTex.h>
